fd3a36d0fc6c17ab5120ef43428be827a1683a65
Lokesh <lokesh152005@gmail.com>
2025-12-09 15:38:16 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials, firestore
 import cloudinary  
 from config import Config
  
-app = Flask(__name__) 
+app = Flask(__name__)  
 app.config.from_object(Config)
 CORS(app) 
 
